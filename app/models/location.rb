6ce3c4d39d89d6f3fbdc49fdccc284074c077bfb
p4nk47w0rk <marc.hellmuth.dd@gmail.com>
2016-04-20 16:23:51 +0200

    canvas object

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,8 @@ class Location < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
+
+  def imageurl(lat , long , zoom)
+      "http://maps.google.com/maps/api/staticmap?size=640x640&sensor=false&zoom=#{zoom}&markers=#{lat}%2C#{long}"
+  end
 end
